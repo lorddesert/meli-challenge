aa7ce8c2c3540f617911c64ee6615a0e0960af7b
lorddesert <sacha.nocetto@hotmail.com>
2024-03-23 22:29:45 -0300

    add react router and update styles

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,28 +1,16 @@
 import './Home.css'
+import { Header } from './Header'
+import { Outlet } from "react-router-dom";
 
-function Home() {
+export function Home() {
 
   return (
     <>
-      <header>
-        <h1>Super Libre Mercado</h1>
-        <form onSubmit={() => { alert('Searched...')}}>
-          <label htmlFor="search">Caja de busqueda</label>
-          <div className="search-bar">
-            <input type="submit" value="" />
-            <button>🔎</button>
-          </div>
-        </form>
-      </header>
-      <main className="search-results">
-        <h2>Resultado de la búsqueda</h2>
-        <div className="breadcrumb"></div>
-        <ul>
-          {/* List of result items */}
-        </ul>
+      <Header />
+      <main>
+        <h1>Super venta libre</h1>
+        <Outlet />
       </main>
     </>
   )
 }
-
-export default Home

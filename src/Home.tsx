40685b5c3fb0d2a921a05a3b51f58d5d0c9c9e49
lorddesert <sacha.nocetto@hotmail.com>
2024-03-24 22:20:14 -0300

    finish home

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,21 @@
 import './Home.css'
 import { Header } from './Header'
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import { Loader } from './Loader';
 
 export function Home() {
+  const navigation = useNavigation()
 
+  if (navigation.state === "loading")
+    return (
+        <>
+          <Header />
+          <main>
+            <h1>Super venta libre</h1>
+            <Loader/>
+          </main>
+        </>
+      )
   return (
     <>
       <Header />

895e2443f84c708c290bfd4fb28901f3d9b2b35b
lorddesert <sacha.nocetto@hotmail.com>
2024-03-24 22:20:56 -0300

    change elements and add Link react router component

--- src/Result.tsx
+++ src/Result.tsx
@@ -1,13 +1,22 @@
+import { Link } from "react-router-dom";
 import { Item } from "./types";
 
 export function Result({ item }: { item: Item }) {
+
+  const ARSPesos = Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: 'ARS',
+    maximumFractionDigits: 0
+});
+
+  const price = ARSPesos.format(item.price)
   return (
-    <a className="result" href={`/items/${item.id}`}>
+    <Link className="result" to={`/items/${item.id}`}>
       <img src={item.thumbnail} alt={`${item.title}`} />
       <div>
-        <h2 className="price">{item.price}</h2>
-        <h3 className="title">{item.title}</h3>
+        <h3 className="price">{price}</h3>
+        <p className="title">{item.title}</p>
       </div>
-    </a>
+    </Link>
   )
 }

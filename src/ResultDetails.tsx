b58c4bc8b14f9c95e1b91fa155c8ca2aae2957c6
lorddesert <sacha.nocetto@hotmail.com>
2024-03-24 22:21:02 -0300

    finish component

--- src/ResultDetails.tsx
+++ src/ResultDetails.tsx
@@ -1,8 +1,48 @@
+import { useLoaderData, useNavigation } from "react-router-dom";
+import { Loader } from "./Loader";
+import { ItemByID } from "./types";
 
 export function ResultDetails() {
+  const { item } = useLoaderData() as { item: ItemByID}
+  const navigation = useNavigation()
+
+  if (navigation.state === "loading") {
+    return <Loader /> 
+  }  
+  
+  const formatter = new Intl.NumberFormat('es-AR', {
+    currency: 'ARS',
+    maximumFractionDigits: 0
+  })
+
+  console.log(item)
+
+  const price = formatter.format(item.price.amount)
+
+function NoDescription() {
+  return <p style={{ opacity: "0.7" }}>El producto aun no tiene descripción.</p>
+}
+
   return (
-    <>
-    Detalles de busqueda
-    </>
+    <article className="result-details">
+      <main>
+        <img src={item.picture.url} alt={`${item.title}`} />
+        <div className="main-info">
+          <p className="stats">{item.condition} - {item.sold_quantity} vendidos</p>
+          <h3>{item.title}</h3>
+          {/* TODO: Should be in teh correct ARS currency format */}
+          <p className="price">
+            <span className="price-icon">$</span>
+            {price}
+            </p>
+          <button className="primary-button">Comprar</button>
+        </div>
+      </main>
+      <footer>
+        <h2>Descripción del producto</h2>
+        <p className="description">{item.description || <NoDescription />}
+        </p>
+      </footer>
+    </article>
   )
 }
